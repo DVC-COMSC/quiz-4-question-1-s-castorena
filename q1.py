096c4b7dbc1a0240a02685ac489f083ea60b96cd
sarah castorena <s.castorena@outlook.com>
2022-10-03 23:32:53 -0700

    New commit

--- q1.py
+++ q1.py
@@ -8,3 +8,19 @@
 # No need to use list
 # All input values are taken one by one separatively.
 ###
+
+# Sarah Castorena
+# 2028049
+amount = 0
+count = 0
+previous = None
+while count <= 10:
+    num = int(input('Enter a number: '))
+    
+    if previous is not None and (previous % 2 == 0) and (num % 2 == 0:)
+        amount = amount + 1 
+   
+
+    count = count + 1 
+previous = num    
+print(amount)
